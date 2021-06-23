147d2bb06a3f3cbf8d47ee5e4539383dd05f302e
JUNYEOP <junyeoop@gmail.com>
2021-06-23 17:27:09 +0900

    Update codes

--- script.js
+++ script.js
@@ -30,115 +30,85 @@ function initialiseGL(canvas) {
 	return true;
 }
 
-// var shaderProgram;
-// let num_vertex = 36;
-var vertexData = [
-	// Backface (RED) -> z = 0.5
-	-0.5, -0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-	0.5,  0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-	0.5, -0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-	-0.5, -0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-	-0.5,  0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-	0.5,  0.5, -0.5,  1.0, 0.0, 0.0, 1.0,
-	// Front (BLUE) -> z = 0.5
-	-0.5, -0.5,  0.5,  0.0, 0.0, 1.0, 1.0,
-	0.5,  0.5,  0.5,  0.0, 0.0, 1.0, 1.0,
-	0.5, -0.5,  0.5,  0.0, 0.0, 1.0, 1.0,
-	-0.5, -0.5,  0.5,  0.0, 0.0, 1.0, 1.0,
-	-0.5,  0.5,  0.5,  0.0, 0.0, 1.0, 1.0,
-	0.5,  0.5,  0.5,  0.0, 0.0, 1.0, 1.0,
-	// LEFT (GREEN) -> z = 0.5
-	-0.5, -0.5, -0.5,  0.0, 1.0, 0.0, 1.0,
-	-0.5,  0.5,  0.5,  0.0, 1.0, 0.0, 1.0,
-	-0.5,  0.5, -0.5,  0.0, 1.0, 0.0, 1.0,
-	-0.5, -0.5, -0.5,  0.0, 1.0, 0.0, 1.0,
-	-0.5, -0.5,  0.5,  0.0, 1.0, 0.0, 1.0,
-	-0.5,  0.5,  0.5,  0.0, 1.0, 0.0, 1.0,
-	// RIGHT (YELLOW) -> z = 0.5
-	0.5, -0.5, -0.5,  1.0, 1.0, 0.0, 1.0,
-	0.5,  0.5,  0.5,  1.0, 1.0, 0.0, 1.0,
-	0.5,  0.5, -0.5,  1.0, 1.0, 0.0, 1.0,
-	0.5, -0.5, -0.5,  1.0, 1.0, 0.0, 1.0,
-	0.5, -0.5,  0.5,  1.0, 1.0, 0.0, 1.0,
-	0.5,  0.5,  0.5,  1.0, 1.0, 0.0, 1.0,
-	// BOTTON (MAGENTA) -> z = 0.5
-	-0.5, -0.5, -0.5,  1.0, 0.0, 1.0, 1.0,
-	0.5, -0.5,  0.5,  1.0, 0.0, 1.0, 1.0,
-	0.5, -0.5, -0.5,  1.0, 0.0, 1.0, 1.0,
-	-0.5, -0.5, -0.5,  1.0, 0.0, 1.0, 1.0,
-	-0.5, -0.5,  0.5,  1.0, 0.0, 1.0, 1.0,
-	0.5, -0.5,  0.5,  1.0, 0.0, 1.0, 1.0,
-	// TOP (CYAN) -> z = 0.5
-	-0.5,  0.5, -0.5,  0.0, 1.0, 1.0, 1.0,
-	0.5,  0.5,  0.5,  0.0, 1.0, 1.0, 1.0,
-	0.5,  0.5, -0.5,  0.0, 1.0, 1.0, 1.0,
-	-0.5,  0.5, -0.5,  0.0, 1.0, 1.0, 1.0,
-	-0.5,  0.5,  0.5,  0.0, 1.0, 1.0, 1.0,
-	0.5,  0.5,  0.5,  0.0, 1.0, 1.0, 1.0
-];
-
-var opacity1 = 1.0;
-var opacity2 = 1.0;
+let opacity = 1.0;
 function makeCube(sx,sy,sz)
 {
 	var vertexData = [
-		// X,Y,Z,  			R,G,B,A,    				U,V,		NX,NY,NZ
-		// Backface (RED/WHITE) -> z = 0.5
-		-sx, -sy, -sz,  	1.0, 0.0, 0.0, opacity1,  	0.0,  0.0,	0, 0, -1,
-		sx,  sy, -sz,  	1.0, 0.0, 0.0, opacity1,  	1.0,  1.0,	0, 0, -1,
-		sx, -sy, -sz,		1.0, 0.0, 0.0, opacity1,  	1.0,  0.0, 	0, 0, -1,
-		-sx, -sy, -sz,		1.0, 0.0, 0.0, opacity2,  	0.0,  0.0, 	0, 0, -1,
-		-sx,  sy, -sz,		1.0, 0.0, 0.0, opacity2,  	0.0,  1.0, 	0, 0, -1,
-		sx,  sy, -sz,		1.0, 0.0, 0.0, opacity2,  	1.0,  1.0, 	0, 0, -1,
-		// Front (BLUE/WHITE) -> z = 0.5
-		-sx, -sy,  sz,		0.0, 0.0, 1.0, opacity1,  	0.0,  1.0, 	0, 0, 1,
-		sx, -sy,  sz,  	0.0, 0.0, 1.0, opacity1,  	1.0,  1.0, 	0, 0, 1,
-		sx,  sy,  sz,  	0.0, 0.0, 1.0, opacity1,  	1.0,  0.0, 	0, 0, 1,
-		-sx, -sy,  sz,  	0.0, 0.0, 1.0, opacity2,  	0.0,  1.0, 	0, 0, 1,
-		sx,  sy,  sz,  	0.0, 0.0, 1.0, opacity2,  	1.0,  0.0, 	0, 0, 1,
-		-sx,  sy,  sz,  	0.0, 0.0, 1.0, opacity2,  	0.0,  0.0, 	0, 0, 1,
-		// LEFT (GREEN/WHITE) -> z = 0.5
-		-sx, -sy, -sz,  	0.0, 1.0, 0.0, opacity2,  	0.0,  0.0, 	-1, 0, 0,
-		-sx,  sy,  sz,  	0.0, 1.0, 0.0, opacity2,  	1.0,  1.0, 	-1, 0, 0,
-		-sx,  sy, -sz,  	0.0, 1.0, 0.0, opacity2,  	1.0,  0.0, 	-1, 0, 0,
-		-sx, -sy, -sz,  	0.0, 1.0, 0.0, opacity1,  	0.0,  0.0, 	-1, 0, 0,
-		-sx, -sy,  sz,  	0.0, 1.0, 0.0, opacity1,  	0.0,  1.0, 	-1, 0, 0,
-		-sx,  sy,  sz,  	0.0, 1.0, 0.0, opacity1,  	1.0,  1.0, 	-1, 0, 0,
-		// RIGHT (YELLOW/WHITE) -> z = 0.5
-		sx, -sy, -sz,  	1.0, 1.0, 0.0, opacity1,  	0.0,  0.0, 	1, 0, 0,
-		sx,  sy, -sz,  	1.0, 1.0, 0.0, opacity1,  	1.0,  0.0, 	1, 0, 0,
-		sx,  sy,  sz,  	1.0, 1.0, 0.0, opacity1,  	1.0,  1.0, 	1, 0, 0,
-		sx, -sy, -sz,  	1.0, 1.0, 0.0, opacity2,  	0.0,  0.0, 	1, 0, 0,
-		sx,  sy,  sz,  	1.0, 1.0, 0.0, opacity2,  	1.0,  1.0, 	1, 0, 0,
-		sx, -sy,  sz,  	1.0, 1.0, 0.0, opacity2,  	0.0,  1.0, 	1, 0, 0,
-		// BOTTON (MAGENTA/WHITE) -> z = 0.5
-		-sx, -sy, -sz,  	1.0, 0.0, 1.0, opacity1,  	0.0,  0.0, 	0, -1, 0,
-		sx, -sy, -sz,  	1.0, 0.0, 1.0, opacity1,  	1.0,  0.0, 	0, -1, 0,
-		sx, -sy,  sz,  	1.0, 0.0, 1.0, opacity1,  	1.0,  1.0, 	0, -1, 0,
-		-sx, -sy, -sz,  	1.0, 0.0, 1.0, opacity2,  	0.0,  0.0, 	0, -1, 0,
-		sx, -sy,  sz,  	1.0, 0.0, 1.0, opacity2,  	1.0,  1.0, 	0, -1, 0,
-		-sx, -sy,  sz,  	1.0, 0.0, 1.0, opacity2,  	0.0,  1.0, 	0, -1, 0,
-		// TOP (CYAN/WHITE) -> z = 0.5
-		-sx,  sy, -sz,  	0.0, 1.0, 1.0, opacity2,  	0.0,  0.0, 	0, 1, 0,
-		sx,  sy,  sz,  	0.0, 1.0, 1.0, opacity2,  	1.0,  1.0, 	0, 1, 0,
-		sx,  sy, -sz,  	0.0, 1.0, 1.0, opacity2,  	1.0,  0.0, 	0, 1, 0,
-		-sx,  sy, -sz,  	0.0, 1.0, 1.0, opacity1,  	0.0,  0.0, 	0, 1, 0,
-		-sx,  sy,  sz,  	0.0, 1.0, 1.0, opacity1,  	0.0,  1.0, 	0, 1, 0,
-		sx,  sy,  sz,  	0.0, 1.0, 1.0, opacity1,  	1.0,  1.0, 	0, 1, 0
+
+		// X,Y,Z,  					R,	G,	B,			A,    	U,		V,		NX,NY,NZ
+
+		// Backface (RED) -> z = 0.5
+		-sx, -sy, -sz,  	1.0, 0.0, 0.0, opacity,  	0.0,  0.0,	0, 0, -1,
+		sx,  sy, -sz,  		1.0, 0.0, 0.0, opacity,  	1.0,  1.0,	0, 0, -1,
+		sx, -sy, -sz,			1.0, 0.0, 0.0, opacity,  	1.0,  0.0, 	0, 0, -1,
+		-sx, -sy, -sz,		1.0, 0.0, 0.0, opacity,  	0.0,  0.0, 	0, 0, -1,
+		-sx,  sy, -sz,		1.0, 0.0, 0.0, opacity,  	0.0,  1.0, 	0, 0, -1,
+		sx,  sy, -sz,			1.0, 0.0, 0.0, opacity,  	1.0,  1.0, 	0, 0, -1,
+		// Front (BLUE) -> z = 0.5
+		-sx, -sy,  sz,		0.0, 0.0, 1.0, opacity,  	0.0,  1.0, 	0, 0, 1,
+		sx, -sy,  sz, 	 	0.0, 0.0, 1.0, opacity,  	1.0,  1.0, 	0, 0, 1,
+		sx,  sy,  sz,  		0.0, 0.0, 1.0, opacity,  	1.0,  0.0, 	0, 0, 1,
+		-sx, -sy,  sz,  	0.0, 0.0, 1.0, opacity,  	0.0,  1.0, 	0, 0, 1,
+		sx,  sy,  sz,  		0.0, 0.0, 1.0, opacity,  	1.0,  0.0, 	0, 0, 1,
+		-sx,  sy,  sz,  	0.0, 0.0, 1.0, opacity,  	0.0,  0.0, 	0, 0, 1,
+		// LEFT (GREEN) -> z = 0.5
+		-sx, -sy, -sz,  	0.0, 1.0, 0.0, opacity,  	0.0,  0.0, 	-1, 0, 0,
+		-sx,  sy,  sz,  	0.0, 1.0, 0.0, opacity,  	1.0,  1.0, 	-1, 0, 0,
+		-sx,  sy, -sz,  	0.0, 1.0, 0.0, opacity,  	1.0,  0.0, 	-1, 0, 0,
+		-sx, -sy, -sz,  	0.0, 1.0, 0.0, opacity,  	0.0,  0.0, 	-1, 0, 0,
+		-sx, -sy,  sz,  	0.0, 1.0, 0.0, opacity,  	0.0,  1.0, 	-1, 0, 0,
+		-sx,  sy,  sz,  	0.0, 1.0, 0.0, opacity,  	1.0,  1.0, 	-1, 0, 0,
+		// RIGHT (YELLOW) -> z = 0.5
+		sx, -sy, -sz,  		1.0, 1.0, 0.0, opacity,  	0.0,  0.0, 	1, 0, 0,
+		sx,  sy, -sz,  		1.0, 1.0, 0.0, opacity,  	1.0,  0.0, 	1, 0, 0,
+		sx,  sy,  sz,  		1.0, 1.0, 0.0, opacity,  	1.0,  1.0, 	1, 0, 0,
+		sx, -sy, -sz,  		1.0, 1.0, 0.0, opacity,  	0.0,  0.0, 	1, 0, 0,
+		sx,  sy,  sz,  		1.0, 1.0, 0.0, opacity,  	1.0,  1.0, 	1, 0, 0,
+		sx, -sy,  sz,  		1.0, 1.0, 0.0, opacity,  	0.0,  1.0, 	1, 0, 0,
+		// BOTTOM (MAGENTA) -> z = 0.5
+		-sx, -sy, -sz,  	1.0, 0.0, 1.0, opacity,  	0.0,  0.0, 	0, -1, 0,
+		sx, -sy, -sz,  		1.0, 0.0, 1.0, opacity,  	1.0,  0.0, 	0, -1, 0,
+		sx, -sy,  sz,  		1.0, 0.0, 1.0, opacity,  	1.0,  1.0, 	0, -1, 0,
+		-sx, -sy, -sz,  	1.0, 0.0, 1.0, opacity,  	0.0,  0.0, 	0, -1, 0,
+		sx, -sy,  sz,  		1.0, 0.0, 1.0, opacity,  	1.0,  1.0, 	0, -1, 0,
+		-sx, -sy,  sz,  	1.0, 0.0, 1.0, opacity,  	0.0,  1.0, 	0, -1, 0,
+		// TOP (CYAN) -> z = 0.5
+		-sx,  sy, -sz,  	0.0, 1.0, 1.0, opacity,  	0.0,  0.0, 	0, 1, 0,
+		sx,  sy,  sz,  		0.0, 1.0, 1.0, opacity,  	1.0,  1.0, 	0, 1, 0,
+		sx,  sy, -sz,  		0.0, 1.0, 1.0, opacity,  	1.0,  0.0, 	0, 1, 0,
+		-sx,  sy, -sz,  	0.0, 1.0, 1.0, opacity,  	0.0,  0.0, 	0, 1, 0,
+		-sx,  sy,  sz,  	0.0, 1.0, 1.0, opacity,  	0.0,  1.0, 	0, 1, 0,
+		sx,  sy,  sz,  		0.0, 1.0, 1.0, opacity,  	1.0,  1.0, 	0, 1, 0
 	];
 	return (new Float32Array(vertexData));
 }
 
+function makeAxis(){
+	let vertexData2 = [
+		//x
+		-1.0,0.0,0.0, 1.0, 0.0, 0.0, 1.0,
+		1.0,0.0,0.0, 1.0, 0.0, 0.0, 1.0,
+		//y
+		0.0,-1.0,0.0, 0.0, 1.0, 0.0, 1.0,
+		0.0,1.0,0.0, 0.0, 1.0, 0.0, 1.0,
+		//z
+		0.0,0.0,-1.0, 0.0, 0.0, 1.0, 1.0,
+		0.0,0.0,1.0, 0.0, 0.0, 1.0, 1.0,
+
+	]
+	return (new Float32Array(vertexData2));
+
+}
+
 function initialiseBuffer() {
-
-	// gl.vertexBuffer = gl.createBuffer();
-	// gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
-	// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
-
 	gl.vertexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
 	makeCube(0.5,0.5,0.5);
 	gl.bufferData(gl.ARRAY_BUFFER, makeCube(0.5,0.5,0.5) , gl.DYNAMIC_DRAW);
+
+	gl.vertexBuffer2 = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer2);
+	gl.bufferData(gl.ARRAY_BUFFER, makeAxis(), gl.DYNAMIC_DRAW);
 
 	return testGLError("initialiseBuffers");
 }
@@ -224,16 +194,9 @@ function initialiseShaders() {
 		alert("Failed to link the program.\n" + gl.getProgramInfoLog(gl.programObject));
 		return false;
 	}
-
 	gl.useProgram(gl.programObject);
-
 	return testGLError("initialiseShaders");
 }
-
-var xRot = 0.0;
-var yRot = 0.0;
-var zRot = 0.0;
-var speedRot = 0.01;
 
 flag_animation = 0;
 function toggleAnimation()
@@ -261,44 +224,43 @@ function fn_update_fov(val)
 	fov_degree = val;
 }
 
-/*================= Mouse events ======================*/
-function initialStep(canvas) {
-
+/*============================ Mouse events =================================*/
+function initialEvent(canvas) {
 	try {
 		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 		gl.viewport(0, 0, canvas.width, canvas.height);
 	}
-
 	catch (event) {
 	}
 
 	var drag=false;
-
 	var original_x, original_y;
-
-	var Downward=function(event) {
-
-		console.log("Downward");
+	var Down = function(event) {
+		console.log("Down");
 		drag=true;
 		original_x = event.pageX;
 		original_y = event.pageY;
 		event.preventDefault();
-
 	};
 
-	var Upward=function(event){
-		console.log("Upward");
+	var Up = function(event){
+		console.log("Up");
 		drag=false;
 	};
 
-	var Moving=function(event) {
+	var Out = function(event){
+		console.log("Out");
+		drag=false;
+	};
+
+	var Move = function(event) {
 		if (!drag) return false;
 
 		var gap_x = event.pageX - original_x;
 		var gap_y = event.pageY - original_y;
 
-		y_rad = y_rad + gap_x*3/canvas.width;
-		x_rad = x_rad + gap_y*3/canvas.height;
+		y_rad = y_rad + gap_x* 2 / canvas.width;
+		x_rad = x_rad + gap_y* 2 / canvas.height;
 
 		original_x = event.pageX;
 		original_y = event.pageY;
@@ -306,67 +268,75 @@ function initialStep(canvas) {
 		event.preventDefault();
 	};
 
-	canvas.addEventListener("mousedown", Downward, false);
-	canvas.addEventListener("mouseup", Upward, false);
-	canvas.addEventListener("mouseout", Upward, false);
-	canvas.addEventListener("mousemove", Moving, false);
-}
-/*=========================rotation================*/
+	canvas.addEventListener("mousedown", Down, false);
+	canvas.addEventListener("mouseup", Up, false);
+	canvas.addEventListener("mouseout", Out, false);
+	canvas.addEventListener("mousemove", Move, false);
 
+	return true;
+}
+/*====================================================================*/
+
+
+
+/*===========================rotation================================*/
 let rotate_flag = 0;
+
 function rotateToggle(){
 	rotate_flag ^= 1;
+	// if(rotate_flag == 1){
+	// 	xRot_2 =0;
+	// 	yRot_2 =0;
+	// 	zRot_2 =0;
+	// }
 	console.log("rotate_flag=", rotate_flag);
 }
 
 let axis_flag = 0;
 function rotateAxis(axis){
 	if(axis == 1){
-		// xRot = xRot + speedRot;
 		axis_flag = 1;
 	}
 	else if(axis == 2){
-		// yRot = yRot + speedRot;
 		axis_flag = 2;
 	}
 	else if(axis == 3){
-		// zRot = zRot + speedRot;
 		axis_flag = 3;
 	}
 	else if(axis == 4){
-		// zRot = zRot + speedRot;
 		axis_flag = 4;
 	}
 	console.log("axis_flag=", axis_flag);
 }
+/*==================================================================*/
 
-// let flag_draw_twice = 0;
-// let twice_x=0.2, twice_y=0.2, twice_z=0.2;
-// function fn_twice_position()
-// {
-//   twice_x = document.getElementById('twice_x').value;
-//   twice_y = document.getElementById('twice_y').value;
-//   twice_z = document.getElementById('twice_z').value;
-// }
-
-
-
-
+let xRot = 0.0;
+let yRot = 0.0;
+let zRot = 0.0;
+let speedRot = 0.01;
 let mMat, vMat, pMat;
+
+let xRot_2 = 0.0;
+let yRot_2 = 0.0;
+let zRot_2 = 0.0;
+
+let xRot_3 = 0.0;
+let yRot_3 = 0.0;
+let zRot_3 = 0.0;
+
+let xRot_4 = 0.0;
+let yRot_4 = 0.0;
+let zRot_4 = 0.0;
+
 let x_rad = 0;
 let y_rad = 0;
-// let idtmat = new Float32Array(16);
 let light_posx = 0.0;
 let light_posy = 0.0;
 let light_posz = 0.0;
-
-
-
 function renderScene() {
-
 	let canvas = document.getElementById("helloapicanvas")
 
-	gl.clearColor(0.5, 0.5, 0.5, 1.0);
+	gl.clearColor(0.8, 0.8, 0.8, 1.0);
 	gl.clearDepth(1.0);										// Added for depth Test
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);	// Added for depth Test
 	gl.enable(gl.DEPTH_TEST);								// Added for depth Test
@@ -375,26 +345,16 @@ function renderScene() {
 	var vMatLocation = gl.getUniformLocation(gl.programObject, "vMat");
 	var pMatLocation = gl.getUniformLocation(gl.programObject, "pMat");
 
-
-
 	pMat = mat4.create();
 	vMat = mat4.create();
 	mMat = mat4.create();
-	// mat4.ortho(pMat, -8.0/6.0, 8.0/6.0, -1, 1, -1, 1);
-	// mat4.frustum(pMat, -8.0/6.0, 8.0/6.0, -1, 1, 0.5, 3);
 
 	mat4.perspective(pMat, fov_degree * 3.141592 / 180.0 , canvas.width/canvas.height , 0.5, 6);
 	mat4.lookAt(vMat, [0,0,-2], [0.0,0.0,0.0], [0,1,0]);
 
-	// mat4.translate(mMat, mMat, [0,0,-1]);
-
-
-
-
 	mat4.rotateX(mMat, mMat, xRot);
 	mat4.rotateY(mMat, mMat, yRot);
 	mat4.rotateZ(mMat, mMat, zRot);
-
 
 	if (flag_animation == 1)
 	{
@@ -405,75 +365,64 @@ function renderScene() {
 
 	}
 
-	// if (rotate_flag == 1){
-	//   console.log("rotate_flag=", rotate_flag);
-	//   if(axis_flag == 1){
-	//     console.log("axis_flag=", axis_flag);
-	//     xRot = xRot + speedRot;
-	//   }
-	//   else if(axis_flag == 2){
-	//     yRot = yRot + speedRot;
-	//   }
-	//   else if(axis_flag == 3){
-	//     zRot = zRot + speedRot;
-	//   }
-	// }
+	mat4.rotateX(mMat, mMat, xRot_2);
+	mat4.rotateY(mMat, mMat, yRot_3);
+	mat4.rotateZ(mMat, mMat, zRot_4);
 
+	let x_count=0;
+	let y_count=0;
+	let z_count=0;
+// X Axis
 	if (rotate_flag == 1 && axis_flag == 1){
 		console.log("axis_flag =",axis_flag);
-		xRot+= speedRot;
-		yRot=0;
-		zRot =0;
+		x_count++;
+		xRot_2 += speedRot;
+		yRot_3 = 0;
+		zRot_4 = 0;
+		// console.log("xRot =",xRot,"yRot =",yRot,"zRot=",zRot);
+		console.log("xRot =",xRot_2,"yRot =",yRot_2,"zRot=",zRot_2);
 
-
-		console.log("xRot =",xRot,"yRot =",yRot,"zRot=",zRot);
 	}
+	// Y Axis
 	else if (rotate_flag == 1 && axis_flag == 2){
-		yRot = yRot + speedRot;
-		xRot= 0;
-		zRot = 0;
-
-		console.log("xRot =",xRot,"yRot =",yRot,"zRot=",zRot);
+		y_count++;
+		yRot_3 = yRot_3 + speedRot;
+		xRot_2 = 0;
+		zRot_4 = 0;
+		console.log("xRot =",xRot_2,"yRot =",yRot_2,"zRot=",zRot_2);
 
 	}
+	// Z Axis
 	else if (rotate_flag == 1 && axis_flag == 3){
-		zRot = zRot + speedRot;
-		xRot= 0;
-		yRot= 0;
-		console.log("xRot =",xRot,"yRot =",yRot,"zRot=",zRot);
+		z_count++;
+		zRot_4 = zRot_4 + speedRot;
+		xRot_2 = 0;
+		yRot_3 = 0;
+		console.log("xRot =",xRot_2,"yRot =",yRot_2,"zRot=",zRot_2);
+
 	}
+	// STOP
 	else if (rotate_flag == 1 && axis_flag == 4){
-		zRot = 0;
 		xRot= 0;
 		yRot= 0;
+		zRot = 0;
+		// xRot_2 -= (speedRot * x_count);
+		// yRot_2 = (speedRot * y_count);
+		// zRot_2 = (speedRot * z_count);
+		x_count = 0;
+		y_count = 0;
+		z_count = 0;
 		console.log("xRot =",xRot,"yRot =",yRot,"zRot=",zRot);
 	}
 
-
-	// if (rotate_flag == 2){
-	//   yRot = yRot + speedRot;
-	// }
-	//
-	// if (rotate_flag == 3){
-	//   zRot = zRot + speedRot;
-	// }
-
-	/**********************추가**************************/
-	// var wldmatrix = new Float32Array(16);
-	// var viwmatrix = new Float32Array(16);
-	// var prjmatrix = new Float32Array(16);
-
-	// let angle = performance.now()/5000*Math.PI;
-	// mat4.identity(idtmat);
-
+	/**********************Drag**************************/
 	mat4.rotateX(mMat, mMat, -x_rad);
 	mat4.rotateY(mMat, mMat, y_rad);
-	/**********************추가**************************/
+	/****************************************************/
 
 	gl.uniformMatrix4fv(mMatLocation, gl.FALSE, mMat );
 	gl.uniformMatrix4fv(vMatLocation, gl.FALSE, vMat );
 	gl.uniformMatrix4fv(pMatLocation, gl.FALSE, pMat );
-
 
 	// Set Light Position
 	gl.uniform3f(gl.getUniformLocation(gl.programObject, "lightPos"), light_posx, light_posy, light_posz);
@@ -498,19 +447,25 @@ function renderScene() {
 
 	gl.drawArrays(draw_mode, 0, 36);
 
-	// var saveMat = mat4.create();
 
-	// if ( flag_draw_twice ) {
-	//   mat4.translate(mMat, mMat, [twice_x, twice_y, twice_z]);
-	//   // mat4.rotateY(mMat, mMat, 3.141592/2.0);	// 90 degree rotate to make different face color
-	//   gl.uniformMatrix4fv(mMatLocation, gl.FALSE, mMat );
-	//   gl.enable(gl.POLYGON_OFFSET_FILL);
-	//   gl.drawArrays(draw_mode, 0, num_vertex);
-	// }
-	if (!testGLError("gl.drawArrays")) {
+	gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer2);
+	gl.enableVertexAttribArray(0);
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 28, 0);
+	gl.enableVertexAttribArray(1);
+	gl.vertexAttribPointer(1, 4, gl.FLOAT, gl.FALSE, 28, 12);
+
+
+
+	if (!testGLError("gl.vertexAttribPointer")) {
 		return false;
 	}
 
+
+	gl.drawArrays(1, 0, 6);
+
+	if (!testGLError("gl.drawArrays")) {
+		return false;
+	}
 	return true;
 }
 
@@ -521,11 +476,6 @@ function main() {
 		return;
 	}
 
-	initialStep(canvas);
-	// if (!initialStep(canvas)) {
-	//   return;
-	// }
-
 	if (!initialiseBuffer()) {
 		return;
 	}
@@ -534,6 +484,9 @@ function main() {
 		return;
 	}
 
+	if (!initialEvent(canvas)) {
+	  return;
+	}
 
 	requestAnimFrame = (function () {
 		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
